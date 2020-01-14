8ce649983fdc4b44dcad0d7edc1861dd39c03310
LUCYC <lchan217@my.bcit.ca>
2020-01-14 14:32:20 -0800

    fourth commit

--- room_painter.py
+++ room_painter.py
@@ -4,3 +4,6 @@ width = float(input("Enter the width of the room in feet: "))
 height = float(input("Enter the height of the room in feet: "))
 coats = float(input("Enter the number of coats of paint: "))
 surface_area = (length*width) + (2*length*height) +(2*width*height)
+coverage_needed = surface_area*coats
+cans_of_paint_required = coverage_needed/COVERAGE
+print("The number of cans needed to paint this room is " + str(cans_of_paint_required))
