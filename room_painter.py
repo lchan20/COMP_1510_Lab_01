4560b22880798575b69288e2d5fefd7cc053d8e2
LUCYC <lchan217@my.bcit.ca>
2020-01-14 14:23:02 -0800

    third commit

--- room_painter.py
+++ room_painter.py
@@ -2,3 +2,4 @@ COVERAGE = 400
 length = float(input("Enter the length of the room in feet: "))
 width = float(input("Enter the width of the room in feet: "))
 height = float(input("Enter the height of the room in feet: "))
+coats = float(input("Enter the number of coats of paint: "))
