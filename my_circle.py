4a9e48dbaa228c26a87690a1b487394de0e000fd
LUCYC <lchan217@my.bcit.ca>
2020-01-14 14:18:20 -0800

    second commit

--- my_circle.py
+++ my_circle.py
@@ -1,6 +1,6 @@
 PI = 3.14159
 radius = 0
-radius = float(input("enter a radius value for a circle"))
+radius = float(input("enter a radius value for a circle: "))
 double_radius = 2*radius
 circumference = 2*PI*radius
 print(circumference)
@@ -10,5 +10,5 @@ double_radius_area = PI*double_radius*double_radius
 double_radius_circumference = 2*PI*double_radius
 circumference_ratio = double_radius_circumference/circumference
 area_ratio = double_radius_area/area
-print("The circumference is increased by ", str(circumference_ratio), "when radius is doubled")
-print("The area is increased by ", str(area_ratio), "When radius is doubled")
+print("The circumference is increased by " + str(circumference_ratio) + " when radius is doubled")
+print("The area is increased by " + str(area_ratio) + " When radius is doubled")
--- room_painter.py
+++ room_painter.py
@@ -1,2 +1,3 @@
 COVERAGE = 400
 length = (float(input("Enter the length of the room in feet: ")))
+width =
