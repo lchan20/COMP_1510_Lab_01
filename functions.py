b0a632a8ee6433126260b5c6fbadd43dd9be3cbf
LUCYC <lchan217@my.bcit.ca>
2020-01-15 11:17:52 -0800

    third commit

--- functions.py
+++ functions.py
@@ -1,5 +1,9 @@
 def format_name(first_name, last_name):
     my_name = first_name+" " + last_name
     return my_name.title()
-print(format_name("lucy","chan"))
+
+
+first_name = str(input("Enter your first name: "))
+last_name = str(input("Enter your family name: "))
+print(format_name(first_name, last_name))
 
