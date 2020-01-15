7fb8e50c6d2959012d03ce830e284ccf6958e1d0
LUCYC <lchan217@my.bcit.ca>
2020-01-15 11:29:55 -0800

    name function final commit

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-def format_name(first_name, last_name):
+def format_name(first, last):
     my_name = first_name+" " + last_name
     full_name = my_name.strip()
     return full_name.title()
@@ -7,4 +7,3 @@ def format_name(first_name, last_name):
 first_name = str(input("Enter your first name: "))
 last_name = str(input("Enter your family name: "))
 print("My full name is " + format_name(first_name, last_name))
-
