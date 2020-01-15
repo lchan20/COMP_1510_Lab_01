b850c5e6bf0c9f98ef07524e93d02a6d1acd9c7a
LUCYC <lchan217@my.bcit.ca>
2020-01-15 14:38:07 -0800

    final commit base conversion

--- functions.py
+++ functions.py
@@ -15,6 +15,24 @@ def this_year(number):
     return int(third_step + 10)
 
 
+def base_conversion():
+    base = int(input("Enter a base you want to convert to: "))
+    max_base_10 = (base ** 4) - 1
+    print("The maximum base 10 number for your chosen base is " + str(max_base_10))
+    num_base_10 = int(input("Enter a number that is less than or equal to " + str(max_base_10) + ": "))
+    last_digit = num_base_10 % base
+    num_base_10 = num_base_10 // base
+    third_digit = num_base_10 % base
+    num_base_10 = num_base_10 // base
+    second_digit = num_base_10 % base
+    num_base_10 = num_base_10 // base
+    first_digit = num_base_10 % base
+    new_num_str = str(first_digit) + str(second_digit) + str(third_digit) + str(last_digit)
+    new_num_int = int(new_num_str)
+    print("The converted base " + str(base) + " number is " + new_num_str)
+    print(new_num_int)
+
+
 def main():
     first_name = input("Enter your first name: ")
     last_name = input("Enter your family name: ")
@@ -23,8 +41,15 @@ def main():
     print("This is " + something + " tripled: " + tripler(something))
     random_number = input("Enter a random number to get the number of the current year: ")
     print("The current year is: " + str(this_year(random_number)))
+    base_conversion()
     print("complete!")
 
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
