0b2be3e79f72b02f31167a203a98ed5858408bdc
LUCYC <lchan217@my.bcit.ca>
2020-01-15 12:53:56 -0800

    this year commit

--- functions.py
+++ functions.py
@@ -16,3 +16,17 @@ def tripler(anything):
 something = input("Enter something you want tripled: ")
 print("This is " + something + " tripled: " + tripler(something))
 
+
+def this_year(number):
+    first_step = int(number)*30
+    second_step = first_step*67
+    third_step = second_step/number
+    return int(third_step + 10)
+
+
+random_number = int(input("Enter a random number to get the number of the current year: "))
+print("The current year is: " + str(this_year(random_number)))
+
+
+
+
