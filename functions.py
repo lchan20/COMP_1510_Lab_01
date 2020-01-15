9a2d8e5c09cb3de08ce71dfcdad5b7935ae308d6
LUCYC <lchan217@my.bcit.ca>
2020-01-15 12:31:47 -0800

    tripler commit

--- functions.py
+++ functions.py
@@ -7,3 +7,12 @@ def format_name(first, last):
 first_name = str(input("Enter your first name: "))
 last_name = str(input("Enter your family name: "))
 print("My full name is " + format_name(first_name, last_name))
+
+
+def tripler(anything):
+    return 3*str(anything)
+
+
+something = input("Enter something you want tripled: ")
+print("This is " + something + " tripled: " + tripler(something))
+
